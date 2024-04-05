33f2318943042cea850d85d3546c54dd7b9b53bf
ZHANGENSEA <149954066+ZHANGENSEA@users.noreply.github.com>
2024-04-05 12:05:46 +0200

    5/4/2024

--- firmware/TP2_SHELL/firmware/Core/Src/main.c
+++ firmware/TP2_SHELL/firmware/Core/Src/main.c
@@ -61,6 +61,8 @@ void MX_FREERTOS_Init(void);
 
 TaskHandle_t handle_TaskShell;
 TaskHandle_t handle_Task_led;
+TaskHandle_t handle_Task_bidon;
+TaskHandle_t handle_vATaskFunction;
 
 static int leddelay = 1;
 
@@ -86,6 +88,19 @@ int fonction(int argc, char ** argv)
 	return 0;
 }
 
+//TP 3.1.5
+void Task_bidon( void *pvParameters)
+{
+	//int tab[1000];
+	for(;;) {
+		//for (int i=0;i<10000;i++){
+		//	tab[i] = i;
+		//	printf("tab[%d]\r\n",tab[i]);
+		}
+	//}
+}
+
+
 //TD 4.2.7
 int addition(int argc, char ** argv)
 {
@@ -93,7 +108,8 @@ int addition(int argc, char ** argv)
 	if (argc != 3)
 	{
 		//Plante proporement si le (ni plus, ni moins)
-		printf("Error, expected 2 args\r\n");
+		//printf("Error, expected 2 args\r\n");
+		Error_Handler();
 		return -1;
 	}
 	else
@@ -128,7 +144,8 @@ int led(int argc, char ** argv)
 	if (argc != 2)
 	{
 		//Plante proporement si le (ni plus, ni moins)
-		printf("Error, expected 1 args\r\n");
+		//printf("Error, expected 1 args\r\n");
+		Error_Handler();
 		return -1;
 	}
 	else
@@ -141,6 +158,29 @@ int led(int argc, char ** argv)
 
 }
 
+//TP 2.4
+
+int spam(int argc, char ** argv)
+{
+	if (argc != 2)
+	{
+		//Plante proporement si le (ni plus, ni moins)
+		//printf("Error, expected 1 args\r\n");
+		Error_Handler();
+		return -1;
+	}
+	else
+	{
+		for (;;)
+		{
+			printf("%s\r\n", argv[1]);
+		}
+	}
+	return 0;
+
+}
+
+
 void TaskShell(void *p)
 {
 	for(;;)
@@ -149,45 +189,95 @@ void TaskShell(void *p)
 		shell_add('f', fonction, "Une fonction inutile");
 		shell_add('a', addition, "Addition");
 		shell_add('l', led, "Clignoter le LED");
+		shell_add('s', spam, "Afficher le message");
 		shell_run();
 	}
+}
+
+
+//TP 3.2.3
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+{
+	/* Empêchez que des interruptions ne viennent perturber le rapport d'erreur. */
+	portDISABLE_INTERRUPTS();
+
+	/* Affichez le nom de la tâche qui a causé le débordement de la pile. */
+	printf("Stack overflow in task: %s\n", pcTaskName);
+
+	/* Bouclez indéfiniment pour que le comportement soit plus facile à détecter avec un débogueur. */
+	for( ;; );
+}
+
+//TP 3.2.4
+
+void vATaskFunction(void *pvParameters)
+{
+	volatile unsigned char ucArray[1024]; // Utilisez une taille qui dépassera sûrement la pile allouée.
+	(void) ucArray; // Pour éviter l'avertissement d'une variable non utilisée.
+
+	// Accédez à l'élément du tableau pour garantir que le compilateur ne supprime pas l'utilisation.
+	ucArray[0] = 0xAA;
+
+	vATaskFunction(NULL);
+
+	for(;;)
+	{
+		printf("Une tache est ici\r\n");
+	}
+
 }
 
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_USART1_UART_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	BaseType_t ret;
+	xTaskCreate(vATaskFunction, "ATask", 128 /* Pile délibérément trop petite */, NULL, 12, NULL);
 
-	xTaskCreate(TaskShell, "TaskShell", 256, NULL, 8, &handle_TaskShell);
-	xTaskCreate(Task_led, "Task_led", 256, NULL, 7, &handle_Task_led);
+	//	xTaskCreate(TaskShell, "TaskShell", 256, NULL, 8, &handle_TaskShell);
+	//	xTaskCreate(Task_led, "Task_led", 256, NULL, 7, &handle_Task_led);
+
+//	for (int z = 0; z < 200; z++) {
+//		ret = xTaskCreate(Task_bidon, "Task_bidon", 256, NULL, 9, &handle_Task_bidon);
+//		if (ret == pdPASS){
+//			printf("la tache %d se cree\r\n", z);
+//		}
+//		else
+//		{
+//			printf("Error, la tache n'est pas cree\r\n");
+//			Error_Handler();
+//		}
+//	}
+
 	vTaskStartScheduler();
 
 	//	shell_init();
@@ -196,76 +286,76 @@ int main(void)
 	//	shell_add('l', led, "Clignoter le LED");
 	//	shell_run();
 	// Comme pour le scheduler, il ne se passe plus rien après cette ligne
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Call init function for freertos objects (in freertos.c) */
-	MX_FREERTOS_Init();
+  /* Call init function for freertos objects (in freertos.c) */
+  MX_FREERTOS_Init();
 
-	/* Start scheduler */
-	osKernelStart();
+  /* Start scheduler */
+  osKernelStart();
 
-	/* We should never get here as control is now taken by the scheduler */
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1)
 	{
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 25;
-	RCC_OscInitStruct.PLL.PLLN = 432;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 432;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Activate the Over-Drive mode
-	 */
-	if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -273,54 +363,56 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  Period elapsed callback in non blocking mode
- * @note   This function is called  when TIM6 interrupt took place, inside
- * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
- * a global variable "uwTick" used as application time base.
- * @param  htim : TIM handle
- * @retval None
- */
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	/* USER CODE BEGIN Callback 0 */
+  /* USER CODE BEGIN Callback 0 */
 
-	/* USER CODE END Callback 0 */
-	if (htim->Instance == TIM6) {
-		HAL_IncTick();
-	}
-	/* USER CODE BEGIN Callback 1 */
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
 
-	/* USER CODE END Callback 1 */
+  /* USER CODE END Callback 1 */
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
-	while (1)
-	{
-	}
-	/* USER CODE END Error_Handler_Debug */
+
+
+		while (1)
+		{
+		}
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
